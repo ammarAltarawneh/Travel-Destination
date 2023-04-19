e9169782522e4effee4add5ed9b7b5cce37aeec2
ammarAltarawneh <tarawnhammar@gmail.com>
2023-04-19 18:45:06 +0000

    done

---
+++ src/components/header/Header.js
@@ -0,0 +1,14 @@
+
+
+function Header(){
+    return (
+  <>
+  
+  <h1 style={{color:'red'}} className={"Heading"}>Travel Destination</h1>
+
+  
+  </>
+    )
+   }
+  
+   export default Header;
---
+++ src/components/tours/Tours.js
@@ -0,0 +1,27 @@
+
+
+function Tours(props) {
+    console.log(props)
+    
+    return (
+        <>
+            <h1>Tours List</h1>
+            {props.dataSent.data.map((tour)=>{
+                return(
+                    <div key={tour.id}>
+                        <h2>{tour.name}</h2>
+                        <img src={tour.image} alt={tour.name}></img>
+                        <br></br>
+                        <br></br>
+                    </div>
+                )
+            })}
+
+
+
+
+        </>
+    )
+}
+
+export default Tours;
