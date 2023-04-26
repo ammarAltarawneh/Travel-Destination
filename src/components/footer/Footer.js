2507fc7742eafaec5db12b65115b6421d87d47a6
ammarAltarawneh <tarawnhammar@gmail.com>
2023-04-26 18:51:08 +0000

    not completed

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -8,8 +8,10 @@ function Footer(){
         <section id="contacts">
             <p2>Contact us:</p2>
             <br></br>
+            {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
             <a href="#" id="contLogo"> Twitter</a>
             <br></br>
+            {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
             <a href="#" id="contLogo"> Facebook</a>
         </section>
     </footer>
